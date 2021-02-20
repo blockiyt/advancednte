1ade819c8e67e070532eada6400d6de7f4afcdac
blockiyt <luca.hess@gmx.net>
2021-02-20 21:17:54 +0100

    added luckperms support and splitted up the configs for later support to permissionsex

--- src/main/java/de/blocki/advancednte/main/Main.java
+++ src/main/java/de/blocki/advancednte/main/Main.java
@@ -21,23 +21,29 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-        System.out.println("  _                _    _____                        _   _ _______ ______        __   ___  ");
-        System.out.println(" | |              | |  |  __ \\                      | \\ | |__   __|  ____|      /_ | / _ \\ ");
-        System.out.println(" | |    _   _  ___| | _| |__) |__ _ __ _ __ ___  ___|  \\| |  | |  | |__    __   _| || | | |");
-        System.out.println(" | |   | | | |/ __| |/ /  ___/ _ \\ '__| '_ ` _ \\/ __| . ` |  | |  |  __|   \\ \\ / / || | | |");
-        System.out.println(" | |___| |_| | (__|   <| |  |  __/ |  | | | | | \\__ \\ |\\  |  | |  | |____   \\ V /| || |_| |");
-        System.out.println(" |______\\__,_|\\___|_|\\_\\_|   \\___|_|  |_| |_| |_|___/_| \\_|  |_|  |______|   \\_/ |_(_)___/ ");
+        System.out.println("              _                               _ _   _ _______ ______ ");
+        System.out.println("     /\\      | |                             | | \\ | |__   __|  ____|");
+        System.out.println("    /  \\   __| |_   ____ _ _ __   ___ ___  __| |  \\| |  | |  | |__   ");
+        System.out.println("   / /\\ \\ / _` \\ \\ / / _` | '_ \\ / __/ _ \\/ _` | . ` |  | |  |  __|  ");
+        System.out.println("  / ____ \\ (_| |\\ V / (_| | | | | (_|  __/ (_| | |\\  |  | |  | |____ ");
+        System.out.println(" /_/    \\_\\__,_| \\_/ \\__,_|_| |_|\\___\\___|\\__,_|_| \\_|  |_|  |______|");
         System.out.println("");
-        System.out.println("LuckPermsNTE by blocki");
+        System.out.println("AdvancedNTE by blocki");
         System.out.println("");
 
-        LuckPerms api = LuckPermsProvider.get();
-        lpApi = api;
-        plugin = this;
         PluginManager pm = this.getServer().getPluginManager();
-        pm.registerEvents(new Listeners(), this);
-        new PlayerNodeChangeListener(this, api).register();
-        //new GroupPrefixChangedListener(this, api).register();
+        plugin = this;
+
+        if(getServer().getPluginManager().getPlugin("LuckPerms").isEnabled()){
+            System.out.println("[AdvancedNTE] Das Plugin LuckPerms wurde gefunden");
+            LuckPerms api = LuckPermsProvider.get();
+            lpApi = api;
+            pm.registerEvents(new Listeners(), this);
+            new PlayerNodeChangeListener(this, api).register();
+            //new GroupPrefixChangedListener(this, api).register();
+            setDefaultConfigLuckperms();
+        }
+
         setDefaultConfig();
     }
 
@@ -46,16 +52,20 @@ public final class Main extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    private void setDefaultConfig(){
-        if(ConfigManager.get("ChatLayout") == null){ ConfigManager.set("ChatLayout", "%PREFIX%%PLAYERNAME%%SUFFIX% §7:§r %MESSAGE%"); }
-        if(ConfigManager.get("DisplayNameLayout") == null){ ConfigManager.set("DisplayNameLayout", "%PREFIX%%PLAYERNAME%%SUFFIX%");}
-        if(ConfigManager.get("MessagePrefix") == null){ ConfigManager.set("MessagePrefix", "§7[§6LP-NTE§7]"); }
+    private void setDefaultConfigLuckperms() {
         if(ConfigManager.get("MessageGroupAdd") == null){ ConfigManager.set("MessageGroupAdd", "You were added to the %GROUPNAME% group!"); }
         if(ConfigManager.get("MessagePrefixAdd") == null){ ConfigManager.set("MessagePrefixUpdate", "You were given the %PREFIX% prefix!"); }
         if(ConfigManager.get("MessageSuffixAdd") == null){ ConfigManager.set("MessagePrefixUpdate", "You were given the %SUFFIX% suffix!"); }
         if(ConfigManager.get("MessageGroupRemove") == null){ ConfigManager.set("MessageGroupRemove", "You are no longer in the %GROUPNAME% group!"); }
         if(ConfigManager.get("MessagePrefixRemove") == null){ ConfigManager.set("MessagePrefixRemove", "You no longer have the %PREFIX% prefix!"); }
         if(ConfigManager.get("MessageSuffixRemove") == null){ ConfigManager.set("MessageSuffixRemove", "You no longer have the %SUFFIX% suffix!\""); }
+
+    }
+
+    private void setDefaultConfig(){
+        if(ConfigManager.get("ChatLayout") == null){ ConfigManager.set("ChatLayout", "%PREFIX%%PLAYERNAME%%SUFFIX% §7:§r %MESSAGE%"); }
+        if(ConfigManager.get("DisplayNameLayout") == null){ ConfigManager.set("DisplayNameLayout", "%PREFIX%%PLAYERNAME%%SUFFIX%");}
+        if(ConfigManager.get("MessagePrefix") == null){ ConfigManager.set("MessagePrefix", "§7[§6LP-NTE§7]"); }
         prefix = ConfigManager.get("MessagePrefix");
     }
 }
