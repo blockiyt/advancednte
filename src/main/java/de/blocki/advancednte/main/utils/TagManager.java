a6e98f4bdb6a1e91142db4d172ca55823fd5f6f4
blockiyt <luca.hess@gmx.net>
2021-02-26 20:38:00 +0100

    fixed user nametag color in 1.16.5 and releases nte for 1.16.5 but works also on 1.8

--- src/main/java/de/blocki/advancednte/main/utils/TagManager.java
+++ src/main/java/de/blocki/advancednte/main/utils/TagManager.java
@@ -3,40 +3,49 @@ package de.blocki.advancednte.main.utils;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TagManager {
 
     public static void setTag(Player player, String prefix, String suffix) {
-        //System.out.println("setTag");
+        //get board
         Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
-        Team team = board.getTeam(player.getName());
-        //System.out.println("team = " + team);
-        //System.out.println("team.getPrefix() = " + team.getPrefix());
 
-        if(Objects.nonNull(team)) team.removePlayer(player);
-
+        //get weight
         LuckPermsProvider.get().getGroupManager().loadAllGroups();
-
-        Optional<Group> optionalInteger = LuckPermsProvider.get().getGroupManager().getLoadedGroups().stream().filter(g -> g.getName().equals(
+        Optional<Group> lpWeight = LuckPermsProvider.get().getGroupManager().getLoadedGroups().stream().filter(g -> g.getName().equals(
                 LuckPermsProvider.get().getUserManager().getUser(player.getUniqueId()).getCachedData().getMetaData().getPrimaryGroup()
         )).findFirst();
+        int weightInt = (lpWeight.filter(group -> group.getWeight().isPresent()).map(group -> group.getWeight().getAsInt()).orElse(0));
 
-        //System.out.println("optionalInteger.isPresent() = " + optionalInteger.isPresent());
-        //System.out.println("i = " + optionalInteger.get().getWeight().getAsInt());
-        int weight = (optionalInteger.isPresent() ? (optionalInteger.get().getWeight().isPresent() ? optionalInteger.get().getWeight().getAsInt() : 0) : 0);
-        String teamName = "000" + weight + player.getUniqueId().toString().split("-")[0] + ThreadLocalRandom.current().nextInt(10);
+        //get team
+        String teamName = "000" + weightInt + player.getUniqueId().toString().split("-")[0] + ThreadLocalRandom.current().nextInt(10);
+        Team team = board.getTeam(teamName);
+        if(team == null) team = board.registerNewTeam(teamName);
 
-        team = Objects.isNull(board.getTeam(teamName)) ? board.registerNewTeam(teamName) : board.getTeam(teamName);
-
+        //prefix
         team.setPrefix(prefix);
+        //suffix
         team.setSuffix(suffix);
+        //color
+        ChatColor color = null;
+        System.out.println("prefix"+prefix);
+        String lastColors = ChatColor.getLastColors(prefix).replace("§", "");
+        System.out.println("lastColors="+lastColors);
+        for (String colorCodeString : lastColors.split("")) {
+            color = ChatColor.getByChar(colorCodeString);
+            if(color != null && color.isColor()) break;
+        }
+        if(color == null || !color.isColor()) color = ChatColor.AQUA; //wenn keien valide farbe bei last colors gefunden wurde, haben wir Aqua als farbe
+        team.setColor(color); //tests mal!!!
+
+        //set player
         team.addPlayer(player);
         player.setScoreboard(board);
     }
